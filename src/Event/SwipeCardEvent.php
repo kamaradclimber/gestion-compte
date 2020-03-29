d0ff2124c8fcf3b13ff72f3351e00e40b4817270
Yoann Laissus <yoann.laissus@gmail.com>
2020-03-29 19:45:52 +0200

    Symfony 4 migration

---
+++ src/Event/SwipeCardEvent.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Event;
+
+use App\Entity\SwipeCard;
+use Symfony\Component\EventDispatcher\Event;
+
+class SwipeCardEvent extends Event
+{
+    const SWIPE_CARD_SCANNED = 'swipe_card.scanned';
+
+    /**
+     * @var SwipeCard
+     */
+    private $swipeCard;
+
+    public function __construct(SwipeCard $swipeCard)
+    {
+        $this->swipeCard = $swipeCard;
+    }
+
+    /**
+     * @return SwipeCard
+     */
+    public function getSwipeCard(): SwipeCard
+    {
+        return $this->swipeCard;
+    }
+}
